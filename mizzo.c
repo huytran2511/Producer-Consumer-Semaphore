f251a5ea547aa46a1294d856b796a01c98e77297
Huy Tran <48108978+huytran2511@users.noreply.github.com>
2020-05-15 17:19:35 -0700

    Update mizzo.c

--- mizzo.c
+++ mizzo.c
@@ -1,17 +1,3 @@
-/**********************
-***********************
-Student 1: Huy Tran
-Red ID: 818608122
-
-Student 2: Hosun Yoo
-Red ID: 819543212
-
-CS-570
-a05
-mizzo.c
-***********************
-***********************/
-
 #include "belt.h"
 #include "producer.h"
 #include "consumer.h"
