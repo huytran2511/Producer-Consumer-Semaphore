040f3171709ae93310bc817154b9befe2d448d7c
Huy Tran <48108978+huytran2511@users.noreply.github.com>
2020-05-15 17:18:47 -0700

    Update consumer.h

--- consumer.h
+++ consumer.h
@@ -1,17 +1,3 @@
-/**********************
-***********************
-Student 1: Huy Tran
-Red ID: 818608122
-
-Student 2: Hosun Yoo
-Red ID: 819543212
-
-CS-570
-a05
-consumer.h
-***********************
-***********************/
-
 #ifndef _CONSUMER_H_
 #define _CONSUMER_H_
 
